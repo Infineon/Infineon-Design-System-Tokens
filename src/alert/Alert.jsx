ce6406d4f678e93d30a4fbf8fd73486823c78f40
Nicole Konegen <konegen@radikant.com>
2022-01-12 16:04:41 +0100

    feat(Alert): add component

---
+++ src/alert/Alert.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import BsAlert from "react-bootstrap/Alert";
+
+const propTypes = {
+  /** Set Alert color */
+  variant: PropTypes.oneOf([
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+  ]),
+
+  /** Specifies a closable alert */
+  closable: PropTypes.bool,
+};
+
+export const Alert = ({ closable, children, ...props }) => {
+  return (
+    <BsAlert {...props} dismissible={closable}>
+      {children}
+    </BsAlert>
+  );
+};
+Alert.propTypes = propTypes;
---
+++ src/alert/Alert.stories.jsx
@@ -0,0 +1,38 @@
+import { Alert } from "./Alert";
+import BsAlert from "react-bootstrap/Alert";
+
+export default {
+  title: "Components/Alert",
+  component: Alert,
+  args: {variant: "primary"}
+};
+
+const DefaultTemplate = (args) => (
+  <Alert {...args}>
+    This is a {args.variant} alert — check it out!
+  </Alert>
+);
+
+const LinkTemplate = (args) => (
+  <Alert {...args}>
+    A simple {args.variant} alert with <BsAlert.Link href="#">an example link</BsAlert.Link>. Give it a click if you like.
+  </Alert>
+);
+
+const AdditionalContentTemplate = (args) => (
+  <Alert {...args}>
+    <BsAlert.Heading>Well done!</BsAlert.Heading>
+    <p>
+      Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.
+    </p>
+    <p className="mb-0">
+      Whenever you need to, be sure to use margin utilities to keep things nice and tidy.
+    </p>
+  </Alert>
+);
+
+export const Default = DefaultTemplate.bind({});
+
+export const Link = LinkTemplate.bind({});
+
+export const AdditionalContent = AdditionalContentTemplate.bind({});

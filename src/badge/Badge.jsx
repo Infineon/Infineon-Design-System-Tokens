0c7c22bf72c895c81084f0fbc65e1b652201bdcf
Nicole Konegen <konegen@radikant.com>
2022-01-13 17:15:37 +0100

    feat(Badge): add component

---
+++ src/badge/Badge.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import BsBadge from "react-bootstrap/Badge";
+
+const propTypes = {
+  /** Set background color */
+  background: PropTypes.oneOf(["primary", "secondary", "success", "danger", "warning", "white"]),
+
+  /** Set text color */
+  text: PropTypes.oneOf(["white", "black"]),
+
+  /** Specifies a pill Badge */
+  pill: PropTypes.bool,
+};
+
+export const Badge = ({ background, children, ...props }) => {
+  return (
+    <BsBadge bg={background} {...props}>{children}</BsBadge>
+  );
+};
+Badge.propTypes = propTypes;
---
+++ src/badge/Badge.stories.jsx
@@ -0,0 +1,98 @@
+import { Badge } from "./Badge";
+import { Button } from "../button/Button";
+
+export default {
+  title: "Components/Badge",
+  component: Badge,
+  args: {pill: false}
+};
+
+const H1Template = (args) => (
+  <h1><Badge {...args}>New</Badge></h1>
+);
+
+const H2Template = (args) => (
+  <h2><Badge {...args}>New</Badge></h2>
+);
+
+const H3Template = (args) => (
+  <h3><Badge {...args}>New</Badge></h3>
+);
+
+const H4Template = (args) => (
+  <h4><Badge {...args}>New</Badge></h4>
+);
+
+const H5Template = (args) => (
+  <h5><Badge {...args}>New</Badge></h5>
+);
+
+const H6Template = (args) => (
+  <h6><Badge {...args}>New</Badge></h6>
+);
+
+const ButtonBadgePrimaryTemplate = (args) => (
+  <Button color = "primary">
+    Notifications <Badge {...args}>4</Badge>
+  </Button>
+);
+
+const ButtonBadgeSecondaryTemplate = (args) => (
+  <Button color = "secondary">
+    Notifications <Badge {...args}>4</Badge>
+  </Button>
+);
+
+export const Default_h1 = H1Template.bind({});
+Default_h1.storyName = "Default <h1>";
+Default_h1.args = {
+  background: "primary",
+  text: "white",
+};
+
+export const Default_h2 = H2Template.bind({});
+Default_h2.storyName = "Default <h2>";
+Default_h2.args = {
+  background: "primary",
+  text: "white",
+};
+
+export const Default_h3 = H3Template.bind({});
+Default_h3.storyName = "Default <h3>";
+Default_h3.args = {
+  background: "primary",
+  text: "white",
+};
+
+export const Default_h4 = H4Template.bind({});
+Default_h4.storyName = "Default <h4>";
+Default_h4.args = {
+  background: "primary",
+  text: "white",
+};
+
+export const Default_h5 = H5Template.bind({});
+Default_h5.storyName = "Default <h5>";
+Default_h5.args = {
+  background: "primary",
+  text: "white",
+};
+
+export const Default_h6 = H6Template.bind({});
+Default_h6.storyName = "Default <h6>";
+Default_h6.args = {
+  background: "primary",
+  text: "white",
+};
+
+export const ButtonBadgePrimary = ButtonBadgePrimaryTemplate.bind({});
+ButtonBadgePrimary.args = {
+  background: "white",
+  text: "black",
+};
+
+export const ButtonBadgeSecondary = ButtonBadgeSecondaryTemplate.bind({});
+ButtonBadgeSecondary.args = {
+  background: "white",
+  text: "black",
+};

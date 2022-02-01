3e789708d068352b64f45cc185c0bafb5f30d3d4
Nicole Konegen <konegen@radikant.com>
2022-02-01 16:47:04 +0100

    feat(Table): add sub-component

--- src/table/Table.jsx
+++ src/table/Table.jsx
@@ -5,7 +5,7 @@ const propTypes = {
   /** Specifies a small Table */
   size: PropTypes.oneOf(["s", "m"]),
 
-  /** Specifies a hoverable table body */
+  /** Specifies a hoverable Table body */
   hover: PropTypes.bool,
 
   /** Specifies a caption text */
@@ -19,28 +19,8 @@ export const Table = ({ size, caption, children, ...props }) => {
 
   return (
     <BsTable size={bsSize} {...props}>
-      {caption ? <caption>{caption}</caption> : ""}
       {children}
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Larry</td>
-          <td>The Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
+      {caption ? <caption>{caption}</caption> : ""}
     </BsTable>
   );
 };
--- src/table/Table.stories.jsx
+++ src/table/Table.stories.jsx
@@ -20,12 +20,33 @@ const DefaultTemplate = (args) => (
         <th>Username</th>
       </tr>
     </thead>
+
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Larry</td>
+        <td>The Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
   </Table>
 );
 
 const HeaderTemplate = (args) => (
   <Table {...args}>
-    <thead className="thead__background-color">
+    <thead className="inf__thead__background-color">
       <tr>
         <th>#</th>
         <th>First Name</th>
@@ -33,6 +54,27 @@ const HeaderTemplate = (args) => (
         <th>Username</th>
       </tr>
     </thead>
+
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Larry</td>
+        <td>The Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
   </Table>
 );
 
@@ -46,6 +88,61 @@ const StripedTemplate = (args) => (
         <th>Username</th>
       </tr>
     </thead>
+
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Larry</td>
+        <td>The Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </Table>
+);
+
+const DefaultNumbersRightTemplate = (args) => (
+  <Table {...args}>
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Username</th>
+        <th className="text-end">#</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td className="text-end">1</td>
+      </tr>
+      <tr>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        <td className="text-end">2</td>
+      </tr>
+      <tr>
+        <td>Larry</td>
+        <td>The Bird</td>
+        <td>@twitter</td>
+        <td className="text-end">3</td>
+      </tr>
+    </tbody>
   </Table>
 );
 
@@ -54,3 +151,5 @@ export const Default = DefaultTemplate.bind({});
 export const Header = HeaderTemplate.bind({});
 
 export const Striped = StripedTemplate.bind({});
+
+export const DefaultNumbersRight = DefaultNumbersRightTemplate.bind({});

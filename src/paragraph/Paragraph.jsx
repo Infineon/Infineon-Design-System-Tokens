a64678e0786084924549a35e6a93723fc572702d
Nicole Konegen <konegen@radikant.com>
2022-02-08 10:04:06 +0100

    feat(Paragraph): extend component + props + add scss

--- src/paragraph/Paragraph.jsx
+++ src/paragraph/Paragraph.jsx
@@ -1,7 +1,78 @@
-export const Paragraph = ({ children }) => {
-  return (
-    <>
-      {children}
-    </>
-  );
+import PropTypes from "prop-types";
+import imageSrc4_3 from "./4-3-s.png";
+import imageSrc16_9 from "./16-9-s.png";
+import imageSrc1_1 from "./1-1-s.png";
+
+const image4_3 = {
+  src: imageSrc4_3,
+  alt: "4-3-s",
 };
+
+const image16_9 = {
+  src: imageSrc16_9,
+  alt: "16-9-s",
+};
+
+const image1_1 = {
+  src: imageSrc1_1,
+  alt: "1-1-s",
+};
+
+const propTypes = {
+  /** Specifies an aspect ratio to use for the image */
+  aspectRatio: PropTypes.oneOf(["4:3", "16:9", "1:1"]),
+
+  /** Specifies the image position */
+  imagePosition: PropTypes.oneOf(["left", "right"]),
+
+  /** Specifies text alignment */
+  alignItems: PropTypes.oneOf(["center", "top"]),
+
+  /** Set the Paragraph title */
+  title: PropTypes.string,
+
+  /** Set the Paragraph text */
+  text: PropTypes.string,
+};
+
+export const Paragraph = ({ children, aspectRatio, imagePosition, alignItems, ...props }) => {
+  const aspectRatioClass = 
+    aspectRatio === "4:3" ? "inf__aspect-ratio--4-3"
+    : aspectRatio === "16:9" ? "inf__aspect-ratio--16-9"
+    : aspectRatio === "1:1" ? "inf__aspect-ratio--1-1"
+    : "";
+
+  const imageSrc =
+    aspectRatio === "4:3" ? image4_3.src
+    : aspectRatio === "16:9" ? image16_9.src
+    : aspectRatio === "1:1" ? image1_1.src
+    : "";
+
+  const imageAlt =
+    aspectRatio === "4:3" ? image4_3.alt
+    : aspectRatio === "16:9" ? image16_9.alt
+    : aspectRatio === "1:1" ? image1_1.alt
+    : "";
+
+  const alignItemsClass =
+    alignItems === "center" ? "align-items-center"
+    : "";
+
+  const Image = () => (
+    <div className="inf__aspect-ratio__box-wrapper">
+      <div className={"inf__aspect-ratio__box " + aspectRatioClass}>
+        <img className="inf__aspect-ratio__box__in" src={imageSrc} alt={imageAlt}/>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className={"d-flex " + alignItemsClass} {...props}>
+      {imagePosition === "left" 
+        ? <><Image/> {children}</>
+        : <>{children} <Image/></>
+      }
+    </div>
+  )
+};
+Paragraph.propTypes = propTypes;
--- src/paragraph/Paragraph.stories.jsx
+++ src/paragraph/Paragraph.stories.jsx
@@ -1,94 +1,54 @@
 import { Paragraph } from "./Paragraph";
 import { Button } from "../button/Button";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default {
   title: "Components/Paragraph",
   component: Paragraph,
+  args: {
+    title: "The standard Lorem Ipsum passage, used since the 1500s",
+    text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.",
+    aspectRatio: "4:3",
+    imagePosition: "right",
+    alignItems: "center",
+  }
 };
 
 const DefaultTemplate = (args) => (
   <Paragraph {...args}>
-    <div className="d-flex align-items-center">
-      <div>
-        <h3>The standard Lorem Ipsum passage, used since the 1500s</h3>
-        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
-        <Button color="primary" href="#">
-          Primary
-        </Button>
-      </div>
-      <img className="w-100" src="holder.js/100px180" alt=""/>
+    <div>
+      <h3>{args.title}</h3>
+      <p>{args.text}</p>
+      <Button color="primary" href="#">
+        Primary
+      </Button>
     </div>
   </Paragraph>
 );
 
-const DefaultCenterRightTemplate = (args) => (
+const BulletpointsTemplate = (args) => (
   <Paragraph {...args}>
-    <div className="d-flex align-items-center">
-      <img className="w-100" src="holder.js/100px180" alt=""/>
-      <div>
-        <h3>The standard Lorem Ipsum passage, used since the 1500s</h3>
-        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
-        <Button color="primary" href="#">
-          Primary
-        </Button>
-      </div>
-    </div>
-  </Paragraph>
-);
-
-const DefaultTopLeftTemplate = (args) => (
-  <Paragraph {...args}>
-    <div className="d-flex">
-      <div>
-        <h3>The standard Lorem Ipsum passage, used since the 1500s</h3>
-        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
-        <Button color="primary" href="#">
-          Primary
-        </Button>
-      </div>
-      <img className="w-100" src="holder.js/100px180" alt=""/>
-    </div>
-  </Paragraph>
-);
-
-const DefaultTopRightTemplate = (args) => (
-  <Paragraph {...args}>
-    <div className="d-flex">
-      <img className="w-100" src="holder.js/100px180" alt=""/>
-      <div>
-        <h3>The standard Lorem Ipsum passage, used since the 1500s</h3>
-        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
-        <Button color="primary" href="#">
-          Primary
-        </Button>
-      </div>
-    </div>
-  </Paragraph>
-);
-
-const DefaultTopLeftBulletpointsTemplate = (args) => (
-  <Paragraph {...args}>
-    <div className="d-flex">
-      <div>
-        <h3>The standard Lorem Ipsum passage, used since the 1500s</h3>
-        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
-        <ul className="list-unstyled">
-          <li>Bear claw cake biscuit</li>
-          <li>Bear claw cake biscuit</li>
-          <li>Bear claw cake biscuit</li>
-        </ul>
-      </div>
-      <img className="w-100" src="holder.js/100px180" alt=""/>
+    <div>
+      <h3>{args.title}</h3>
+      <p>{args.text}</p>
+      <ul className="list-unstyled">
+        <li>
+          <FontAwesomeIcon icon={["fa", "check"]}/>
+          Bear claw cake biscuit
+        </li>
+        <li>
+          <FontAwesomeIcon icon={["fa", "check"]}/>
+          Bear claw cake biscuit
+        </li>
+        <li>
+          <FontAwesomeIcon icon={["fa", "check"]}/>
+          Bear claw cake biscuit
+        </li>
+      </ul>
     </div>
   </Paragraph>
 );
 
 export const Default = DefaultTemplate.bind({});
 
-export const CenterRight = DefaultCenterRightTemplate.bind({});
-
-export const TopLeft = DefaultTopLeftTemplate.bind({});
-
-export const TopRight = DefaultTopRightTemplate.bind({});
-
-export const TopLeftBulletpoints = DefaultTopLeftBulletpointsTemplate.bind({});
+export const Bulletpoints = BulletpointsTemplate.bind({});

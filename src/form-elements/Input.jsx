1e9702a8407756e036659ebfd4cf2ad31b49562d
Nicole Konegen <konegen@radikant.com>
2022-02-28 09:26:39 +0100

    feat(Form-elements): add Input success, error prop

--- src/form-elements/Input.jsx
+++ src/form-elements/Input.jsx
@@ -13,17 +13,25 @@ const propTypes = {
 
   /** Set a disabled Input field */
   disabled: PropTypes.bool,
+
+  /** Set an Input field state */
+  state: PropTypes.oneOf(["none", "success", "error"]),
 };
 
-export const Input = ({ label, labelPosition, size, children, ...props }) => {
+export const Input = ({ label, labelPosition, size, state, children, ...props }) => {
   const bsSize =
     size === "s" ? "sm" 
     : size === "l" ? "lg" 
     : undefined;
 
+  const stateClass =
+    state === "success" ? "inf__input--success"
+    : state === "error" ? "inf__input--error"
+    : "";
+
   if ( labelPosition === "top" ) {
     return (
-      <BsForm.Group className="mb-3" controlId="input">
+      <BsForm.Group className={"mb-3 " + stateClass} controlId="input">
         <BsForm.Label>{label}</BsForm.Label>
         <BsForm.Control placeholder="Placeholder" size={bsSize} {...props}></BsForm.Control>
         <BsForm.Text>
@@ -33,7 +41,7 @@ export const Input = ({ label, labelPosition, size, children, ...props }) => {
     );
   } else {
     return (
-      <BsForm.Group className="inf__input-left mb-3" controlId="input">
+      <BsForm.Group className={"inf__input-left mb-3 " + stateClass} controlId="input">
         <div className="inf__input-left__container">
           <BsForm.Label>{label}</BsForm.Label>
           <BsForm.Control placeholder="Placeholder" size={bsSize} {...props}></BsForm.Control>

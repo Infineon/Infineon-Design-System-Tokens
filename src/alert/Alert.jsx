27da197baf35d797ccf4775ec1fa5a748d1b4c5b
Nicole Konegen <konegen@radikant.com>
2022-04-05 21:16:02 +0200

    feat(Alert): add light icons

--- src/alert/Alert.jsx
+++ src/alert/Alert.jsx
@@ -20,26 +20,117 @@ const propTypes = {
   icon: PropTypes.bool,
 };
 
-export const Alert = ({ closable, icon, children, ...props }) => {
-  const Icon = () => (
+export const Alert = ({ variant, closable, icon, children, ...props }) => {
+  const IconInfo = () => (
     icon ? <FontAwesomeIcon icon={["fal", "circle-info"]} className="ifx__alert-icon"/> : null
   );
 
+  const IconSuccess = () => (
+    icon ? <FontAwesomeIcon icon={["fal", "circle-check"]} className="ifx__alert-icon"/> : null
+  );
+
+  const IconDanger = () => (
+    icon ? <FontAwesomeIcon icon={["fal", "triangle-exclamation"]} className="ifx__alert-icon"/> : null
+  );
+
+  const IconWarning = () => (
+    icon ? <FontAwesomeIcon icon={["fal", "circle-exclamation"]} className="ifx__alert-icon"/> : null
+  );
+
   if ( icon ) {
-    return (
-      <BsAlert {...props} dismissible={closable}>
-        <div className="ifx__alert-icon-wrapper">
-          <Icon/>
-        </div>
-        {children}
-      </BsAlert>
-    )
+    if ( variant === "primary" ) {
+      return (
+        <BsAlert {...props} variant="primary" dismissible={closable}>
+          <div className="ifx__alert-icon-wrapper">
+            <IconInfo/>
+          </div>
+          {children}
+        </BsAlert>
+      )
+    } else if ( variant === "secondary" ) {
+      return (
+        <BsAlert {...props} variant="secondary" dismissible={closable}>
+          <div className="ifx__alert-icon-wrapper">
+            <IconInfo/>
+          </div>
+          {children}
+        </BsAlert>
+      )
+    } else if ( variant === "success" ) {
+      return (
+        <BsAlert {...props} variant="success" dismissible={closable}>
+          <div className="ifx__alert-icon-wrapper">
+            <IconSuccess/>
+          </div>
+          {children}
+        </BsAlert>
+      )
+    } else if ( variant === "danger" ) {
+      return (
+        <BsAlert {...props} variant="danger" dismissible={closable}>
+          <div className="ifx__alert-icon-wrapper">
+            <IconDanger/>
+          </div>
+          {children}
+        </BsAlert>
+      )
+    } else if ( variant === "warning" ) {
+      return (
+        <BsAlert {...props} variant="warning" dismissible={closable}>
+          <div className="ifx__alert-icon-wrapper">
+            <IconWarning/>
+          </div>
+          {children}
+        </BsAlert>
+      )
+    } else if ( variant === "color-gray-100" ) {
+      return (
+        <BsAlert {...props} variant="color-gray-100" dismissible={closable}>
+          <div className="ifx__alert-icon-wrapper">
+            <IconInfo/>
+          </div>
+          {children}
+        </BsAlert>
+      )
+    }
   } else {
-    return (
-      <BsAlert {...props} dismissible={closable}>
-        {children}
-      </BsAlert>
-    )
+    if ( variant === "primary" ) {
+      return (
+        <BsAlert {...props} variant="primary" dismissible={closable}>
+          {children}
+        </BsAlert>
+      )
+    } else if ( variant === "secondary" ) {
+      return (
+        <BsAlert {...props} variant="secondary" dismissible={closable}>
+          {children}
+        </BsAlert>
+      )
+    } else if ( variant === "success" ) {
+      return (
+        <BsAlert {...props} variant="success" dismissible={closable}>
+          {children}
+        </BsAlert>
+      )
+    } else if ( variant === "danger" ) {
+      return (
+        <BsAlert {...props} variant="danger" dismissible={closable}>
+          {children}
+        </BsAlert>
+      )
+    } else if ( variant === "warning" ) {
+      return (
+        <BsAlert {...props} variant="warning" dismissible={closable}>
+          {children}
+        </BsAlert>
+      )
+    } else if ( variant === "color-gray-100" ) {
+      return (
+        <BsAlert {...props} variant="color-gray-100" dismissible={closable}>
+          {children}
+        </BsAlert>
+      )
+    }
   }
 };
 Alert.propTypes = propTypes;

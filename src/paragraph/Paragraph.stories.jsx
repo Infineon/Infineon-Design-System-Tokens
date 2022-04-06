82a08ac6c713b50b86c692ee09dc01e22ca88828
Nicole Konegen <konegen@radikant.com>
2022-04-06 17:43:55 +0200

    feat(Paragraph): add Bulletpoints as sub-component

--- src/paragraph/Paragraph.stories.jsx
+++ src/paragraph/Paragraph.stories.jsx
@@ -1,4 +1,5 @@
 import { Paragraph } from "./Paragraph";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default {
   title: "Components/Paragraph",
@@ -11,19 +12,23 @@ export default {
 
 const DefaultTemplate = (args) => (
   <Paragraph {...args}>
-    <p>{args.text}</p>
+    <div className="ifx__paragraph-without-icon">
+      <p>{args.text}</p>
+    </div>
   </Paragraph>
 );
 
 const TwoColumnsTemplate = (args) => (
   <Paragraph {...args}>
-    <p className="inf__paragraph__columns">{args.text}</p>
+    <div className="ifx__paragraph-without-icon">
+      <p className="inf__paragraph__columns">{args.text}</p>
+    </div>
   </Paragraph>
 );
 
 const ArticleTemplate = (args) => (
   <Paragraph {...args}>
-    <div>
+    <div className="ifx__paragraph-without-icon">
       <div className="row">
         <div className="col-6">
         </div>
@@ -32,6 +37,78 @@ const ArticleTemplate = (args) => (
         <div className="col-7">
           <p>{args.text}</p>
         </div>
+      </div>
+    </div>
+  </Paragraph>
+);
+
+const Bulletpoints2Template = (args) => (
+  <Paragraph {...args}>
+    <div className="row ifx__bulletpoints">
+      <div className="col-md-6">
+        <FontAwesomeIcon icon={["fal", "check"]}/>
+        <h5>Collumn Headline</h5>
+        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
+      </div>
+
+      <div className="col-md-6">
+        <FontAwesomeIcon icon={["fal", "check"]}/>
+        <h5>Collumn Headline</h5>
+        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
+      </div>
+    </div>
+  </Paragraph>
+);
+
+const Bulletpoints3Template = (args) => (
+  <Paragraph {...args}>
+    <div className="row ifx__bulletpoints">
+      <div className="col-md-4">
+        <FontAwesomeIcon icon={["fal", "check"]}/>
+        <h5>Collumn Headline</h5>
+        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
+      </div>
+
+      <div className="col-md-4">
+        <FontAwesomeIcon icon={["fal", "check"]}/>
+        <h5>Collumn Headline</h5>
+        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
+      </div>
+
+      <div className="col-md-4">
+        <FontAwesomeIcon icon={["fal", "check"]}/>
+        <h5>Collumn Headline</h5>
+        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
+      </div>
+    </div>
+  </Paragraph>
+);
+
+const Bulletpoints4Template = (args) => (
+  <Paragraph {...args}>
+    <div className="row ifx__bulletpoints">
+      <div className="col-md-3">
+        <FontAwesomeIcon icon={["fal", "check"]}/>
+        <h5>Collumn Headline</h5>
+        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
+      </div>
+
+      <div className="col-md-3">
+        <FontAwesomeIcon icon={["fal", "check"]}/>
+        <h5>Collumn Headline</h5>
+        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
+      </div>
+
+      <div className="col-md-3">
+        <FontAwesomeIcon icon={["fal", "check"]}/>
+        <h5>Collumn Headline</h5>
+        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
+      </div>
+
+      <div className="col-md-3">
+        <FontAwesomeIcon icon={["fal", "check"]}/>
+        <h5>Collumn Headline</h5>
+        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</p>
       </div>
     </div>
   </Paragraph>
@@ -51,3 +128,9 @@ export const Article = ArticleTemplate.bind({});
 Article.args = {
   text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.",
 };
+
+export const Bulletpoints2 = Bulletpoints2Template.bind({});
+
+export const Bulletpoints3 = Bulletpoints3Template.bind({});
+
+export const Bulletpoints4 = Bulletpoints4Template.bind({});

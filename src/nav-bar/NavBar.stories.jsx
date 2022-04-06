6a3a4f32495fd33d495e42bcc4e1253d680bc77d
Nicole Konegen <konegen@radikant.com>
2022-04-06 16:28:47 +0200

    feat(Nav-bar): add vertical sub-component

--- src/nav-bar/NavBar.stories.jsx
+++ src/nav-bar/NavBar.stories.jsx
@@ -44,4 +44,42 @@ const DefaultTemplate = (args) => (
   </NavBar>
 );
 
+const VerticalTemplate = (args) => (
+  <div className="inf__vertical-nav-icons-wrapper">
+    <ul className="flex-column inf__content-nav__border nav" {...args}>
+      <BsNav.Item as="li">
+        <BsNav.Link href="#">
+          <FontAwesomeIcon icon={["fas", "globe"]} />
+        </BsNav.Link>
+      </BsNav.Item>
+      <BsNav.Item as="li">
+        <BsNav.Link eventKey="link-1">
+          <FontAwesomeIcon icon={["fas", "compass"]} />
+        </BsNav.Link>
+      </BsNav.Item>
+      <BsNav.Item as="li">
+        <BsNav.Link eventKey="link-2">
+          <FontAwesomeIcon icon={["fas", "chart-bar"]} />
+        </BsNav.Link>
+      </BsNav.Item>
+      <p className="inf__content-nav__divider">Divider</p>
+      <BsNav.Item as="li">
+        <BsNav.Link eventKey="link-3">
+          <FontAwesomeIcon icon={["fas", "home"]} />
+        </BsNav.Link>
+      </BsNav.Item>
+      <BsNav.Item as="li">
+        <BsNav.Link eventKey="link-4">
+          <FontAwesomeIcon icon={["fas", "tachometer-alt"]} />
+        </BsNav.Link>
+      </BsNav.Item>
+    </ul>
+    <div className="d-inline-flex inf__content-nav__border inf__vertical-nav-icons-btn">
+      <FontAwesomeIcon icon={["fas", "chevron-right"]} />
+    </div>
+  </div>
+);
+
 export const Default = DefaultTemplate.bind({});
+
+export const Vertical = VerticalTemplate.bind({});

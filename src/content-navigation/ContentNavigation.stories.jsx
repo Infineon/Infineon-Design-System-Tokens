3cbf950ca382243d0af0c464e2c7486d7ca579f0
Nicole Konegen <konegen@radikant.com>
2022-04-06 16:58:30 +0200

    feat(Content Nav): delete sub-components

--- src/content-navigation/ContentNavigation.stories.jsx
+++ src/content-navigation/ContentNavigation.stories.jsx
@@ -1,7 +1,6 @@
 import { ContentNavigation } from "./ContentNavigation";
 import BsNav from "react-bootstrap/Nav";
 import BsNavDropdown from "react-bootstrap/NavDropdown";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default {
   title: "Components/Content Navigation",
@@ -15,32 +14,6 @@ const DefaultTemplate = (args) => (
     </BsNav.Item>
     <BsNav.Item as="li">
       <BsNav.Link eventKey="link-1">Link default</BsNav.Link>
-    </BsNav.Item>
-    <BsNav.Item as="li">
-      <BsNav.Link eventKey="link-2">Link default</BsNav.Link>
-    </BsNav.Item>
-    <BsNavDropdown as="li" title="Link dropdown" id="nav-dropdown">
-      <BsNavDropdown.Item eventKey="4.1">Action</BsNavDropdown.Item>
-      <BsNavDropdown.Item eventKey="4.2">Another action</BsNavDropdown.Item>
-      <BsNavDropdown.Item eventKey="4.3">Something else here</BsNavDropdown.Item>
-      <BsNavDropdown.Divider />
-      <BsNavDropdown.Item eventKey="4.4">Separated link</BsNavDropdown.Item>
-    </BsNavDropdown>
-    <BsNav.Item as="li">
-      <BsNav.Link eventKey="disabled" disabled>
-        Disabled
-      </BsNav.Link>
-    </BsNav.Item>
-  </ContentNavigation>
-);
-
-const TabsTemplate = (args) => (
-  <ContentNavigation {...args} variant="tabs">
-    <BsNav.Item as="li">
-      <BsNav.Link href="/home">Link active</BsNav.Link>
-    </BsNav.Item>
-    <BsNav.Item as="li">
-      <BsNav.Link eventKey="link-2">Link default</BsNav.Link>
     </BsNav.Item>
     <BsNav.Item as="li">
       <BsNav.Link eventKey="link-2">Link default</BsNav.Link>
@@ -112,32 +85,6 @@ const DefaultVerticalTemplate = (args) => (
   </ContentNavigation>
 );
 
-const VerticalTemplate = (args) => (
-  <ContentNavigation className="inf__content-nav-default inf__content-nav-default-vertical flex-column" {...args}>
-    <BsNav.Item as="li">
-      <BsNav.Link href="/home">Link active</BsNav.Link>
-    </BsNav.Item>
-    <BsNav.Item as="li">
-      <BsNav.Link eventKey="link-1">Link default</BsNav.Link>
-    </BsNav.Item>
-    <BsNav.Item as="li">
-      <BsNav.Link eventKey="link-2">Link default</BsNav.Link>
-    </BsNav.Item>
-    <BsNavDropdown as="li" title="Link dropdown" id="nav-dropdown">
-      <BsNavDropdown.Item eventKey="4.1">Action</BsNavDropdown.Item>
-      <BsNavDropdown.Item eventKey="4.2">Another action</BsNavDropdown.Item>
-      <BsNavDropdown.Item eventKey="4.3">Something else here</BsNavDropdown.Item>
-      <BsNavDropdown.Divider />
-      <BsNavDropdown.Item eventKey="4.4">Separated link</BsNavDropdown.Item>
-    </BsNavDropdown>
-    <BsNav.Item as="li">
-      <BsNav.Link eventKey="disabled" disabled>
-        Disabled
-      </BsNav.Link>
-    </BsNav.Item>
-  </ContentNavigation>
-);
-
 const PillsVerticalTemplate = (args) => (
   <ContentNavigation {...args} variant="pills flex-column">
     <BsNav.Item as="li">
@@ -164,52 +111,10 @@ const PillsVerticalTemplate = (args) => (
   </ContentNavigation>
 );
 
-const VerticalNavIconsTemplate = (args) => (
-  <div className="inf__vertical-nav-icons-wrapper">
-    <ContentNavigation className="flex-column inf__content-nav__border" {...args}>
-        <BsNav.Item as="li">
-          <BsNav.Link href="#">
-            <FontAwesomeIcon icon={["fas", "globe"]} />
-          </BsNav.Link>
-        </BsNav.Item>
-        <BsNav.Item as="li">
-          <BsNav.Link eventKey="link-1">
-            <FontAwesomeIcon icon={["fas", "compass"]} />
-          </BsNav.Link>
-        </BsNav.Item>
-        <BsNav.Item as="li">
-          <BsNav.Link eventKey="link-2">
-            <FontAwesomeIcon icon={["fas", "chart-bar"]} />
-          </BsNav.Link>
-        </BsNav.Item>
-        <p className="inf__content-nav__divider">Divider</p>
-        <BsNav.Item as="li">
-          <BsNav.Link eventKey="link-3">
-            <FontAwesomeIcon icon={["fas", "home"]} />
-          </BsNav.Link>
-        </BsNav.Item>
-        <BsNav.Item as="li">
-          <BsNav.Link eventKey="link-4">
-            <FontAwesomeIcon icon={["fas", "tachometer-alt"]} />
-          </BsNav.Link>
-        </BsNav.Item>
-    </ContentNavigation>
-    <div className="d-inline-flex inf__content-nav__border inf__vertical-nav-icons-btn">
-      <FontAwesomeIcon icon={["fas", "chevron-right"]} />
-    </div>
-  </div>
-);
-
 export const Default = DefaultTemplate.bind({});
-
-export const Tabs = TabsTemplate.bind({});
 
 export const Pills = PillsTemplate.bind({});
 
 export const DefaultVertical = DefaultVerticalTemplate.bind({});
 
-export const Vertical = VerticalTemplate.bind({});
-
 export const PillsVertical = PillsVerticalTemplate.bind({});
-
-export const VerticalNavIcons = VerticalNavIconsTemplate.bind({});

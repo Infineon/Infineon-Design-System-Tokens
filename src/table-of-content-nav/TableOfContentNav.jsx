07776b98ff1c75550db29005d5ac40e2e730d7da
Nicole Konegen <konegen@radikant.com>
2022-04-06 17:04:56 +0200

    feat(Table of Content Nav): delete component

--- src/table-of-content-nav/TableOfContentNav.jsx
+++
@@ -1,9 +0,0 @@
-import BsNav from "react-bootstrap/Nav";
-
-export const TableOfContentNav = ({ children, ...props }) => {
-  return (
-    <BsNav {...props} activeKey="link-1" as="ul" className="inf__table-of-content-nav flex-column sticky-top">
-      {children}
-    </BsNav>
-  );
-};

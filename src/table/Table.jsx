839ecbe2f9f68daa0a29bbec24d6793364836148
Nicole Konegen <konegen@radikant.com>
2022-04-29 17:37:59 +0200

    feat(Table): delete sub-component + add textAlign prop + add styling

--- src/table/Table.jsx
+++ src/table/Table.jsx
@@ -10,17 +10,52 @@ const propTypes = {
 
   /** Specifies a caption text */
   caption: PropTypes.string,
+
+  /** Specifies text alignment */
+  textAlign: PropTypes.oneOf(["left", "right"]),
 };
 
-export const Table = ({ size, caption, children, ...props }) => {
+export const Table = ({ size, caption, textAlign, children, ...props }) => {
   const bsSize =
     size === "s" ? "sm" 
     : undefined;
+
+  const textAlignClass =
+  textAlign === "left" ? ""
+  : "text-end";
 
   return (
     <div class="table-responsive">
       <BsTable size={bsSize} {...props}>
         {children}
+        
+        <tbody className={textAlignClass}>
+          <tr>
+            <td>1</td>
+            <td>15</td>
+            <td>15</td>
+            <td>Example</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>21</td>
+            <td>21</td>
+            <td>Example</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>12</td>
+            <td>12</td>
+            <td>Example</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>25</td>
+            <td>25</td>
+            <td>Example</td>
+          </tr>
+        </tbody>
+
         {caption ? <caption>{caption}</caption> : ""}
       </BsTable>
     </div>
--- src/table/Table.stories.jsx
+++ src/table/Table.stories.jsx
@@ -7,6 +7,7 @@ export default {
     size: "m",
     hover: false,
     caption: "",
+    textAlign: "left",
   }
 };
 
@@ -15,38 +16,11 @@ const DefaultTemplate = (args) => (
     <thead>
       <tr>
         <th>#</th>
-        <th>First</th>
-        <th>Last</th>
-        <th>Handle</th>
+        <th>First item</th>
+        <th>Second item</th>
+        <th>Last item</th>
       </tr>
     </thead>
-
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>15</td>
-        <td>15</td>
-        <td>Name example</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>21</td>
-        <td>21</td>
-        <td>Name example</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>12</td>
-        <td>12</td>
-        <td>Name example</td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>25</td>
-        <td>25</td>
-        <td>Name example</td>
-      </tr>
-    </tbody>
   </Table>
 );
 
@@ -55,38 +29,11 @@ const HeaderTemplate = (args) => (
     <thead className="ifx__thead__background-color">
       <tr>
         <th>#</th>
-        <th>First</th>
-        <th>Last</th>
-        <th>Handle</th>
+        <th>First item</th>
+        <th>Second item</th>
+        <th>Last item</th>
       </tr>
     </thead>
-
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>15</td>
-        <td>15</td>
-        <td>Name example</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>21</td>
-        <td>21</td>
-        <td>Name example</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>12</td>
-        <td>12</td>
-        <td>Name example</td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>25</td>
-        <td>25</td>
-        <td>Name example</td>
-      </tr>
-    </tbody>
   </Table>
 );
 
@@ -95,78 +42,11 @@ const StripedTemplate = (args) => (
     <thead>
       <tr>
         <th>#</th>
-        <th>First</th>
-        <th>Last</th>
-        <th>Handle</th>
+        <th>First item</th>
+        <th>Second item</th>
+        <th>Last item</th>
       </tr>
     </thead>
-
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>15</td>
-        <td>15</td>
-        <td>Name example</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>21</td>
-        <td>21</td>
-        <td>Name example</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>12</td>
-        <td>12</td>
-        <td>Name example</td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>25</td>
-        <td>25</td>
-        <td>Name example</td>
-      </tr>
-    </tbody>
-  </Table>
-);
-
-const DefaultNumbersRightTemplate = (args) => (
-  <Table {...args}>
-    <thead>
-      <tr>
-        <th>First</th>
-        <th>Last</th>
-        <th>Handle</th>
-        <th className="text-end">#</th>
-      </tr>
-    </thead>
-
-    <tbody>
-      <tr>
-        <td>15</td>
-        <td>15</td>
-        <td>Name example</td>
-        <td className="text-end">1</td>
-      </tr>
-      <tr>
-        <td>21</td>
-        <td>21</td>
-        <td>Name example</td>
-        <td className="text-end">2</td>
-      </tr>
-      <tr>
-        <td>12</td>
-        <td>12</td>
-        <td>Name example</td>
-        <td className="text-end">3</td>
-      </tr>
-      <tr>
-        <td>25</td>
-        <td>25</td>
-        <td>Name example</td>
-        <td className="text-end">4</td>
-      </tr>
-    </tbody>
   </Table>
 );
 
@@ -175,5 +55,3 @@ export const Default = DefaultTemplate.bind({});
 export const Header = HeaderTemplate.bind({});
 
 export const Striped = StripedTemplate.bind({});
-
-export const DefaultNumbersRight = DefaultNumbersRightTemplate.bind({});
